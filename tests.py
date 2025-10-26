7c939dd7d26b0da97dc7135102c71a297d483250
kamilachka2909-ctrl <kamilachka2909@gmail.com>
2025-10-26 21:40:06 +0300

    Sprint_4

--- tests.py
+++ tests.py
@@ -1,28 +1,20 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        # Добавляем две разные книги
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_new_book('Шерлок Холмс')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # Проверяем, что обе книги добавлены
+        assert len(collector.books_genre) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-       
+
     @pytest.mark.parametrize(
         'name, genre',
         [
@@ -36,7 +28,7 @@ class TestBooksCollector:
     def test_set_book_genre_valid_input_genre_assigned(self, name, genre):
         collector = BooksCollector()
 
-        # Добавляем книгу и устанавливаем корректный жанр
+        # Добавляем новую книгу и устанавливаем книге жанр
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
 
